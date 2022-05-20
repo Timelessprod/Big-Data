7e670d566787abe754e733caca870dfae92cdb01
Adrien Merat <adrien.merat@epita.fr>
2022-05-20 18:31:22 +0200

    [EDIT] Add `duration_between_rows()`

--- tmp.py
+++ tmp.py
@@ -1,5 +1,8 @@
+import pyspark
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from pyspark.sql.window import Window
 
 
 def create_spark_session(name: str) -> SparkSession:
@@ -20,9 +23,20 @@ def load_data(path: str) -> DataFrame:
     return spark.read.csv(path, dev_schema, header=True)
 
 
+def duration_between_rows(df: DataFrame):
+    df = df.orderBy('Date')
+    df = df.withColumn('duration',
+                       datediff(df['Date'],
+                                lag(df['Date'], 1).over(Window.partitionBy("company_name").orderBy('Date')))
+                       )
+    return df.select(mean('duration')).collect()[0][0]
+
+
 if __name__ == "__main__":
     spark = create_spark_session("Spark_Application_Name")
     df = load_data('stocks_data/AMAZON.csv')
 
     df.printSchema()
     df.show(40)
+    df.count()
+    print(duration_between_rows(df))
