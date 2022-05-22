919d9459a3682918f0ad0750a18f4ed23d875ad2
mehdi "Arcanite" Oueslati <mehdi.oueslati@epita.fr>
2022-05-22 15:19:44 +0200

    adding yesterday's work, missing candlestick from Manu

--- tmp.py
+++ tmp.py
@@ -1,13 +1,17 @@
 """
 load dataframe from a csv
 """
+import time
+import datetime
+from math import sqrt
+
 from pyspark.ml.linalg import DenseMatrix
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.stat import Correlation
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.types import TimestampType, DoubleType, LongType, \
-        StringType, StructField, StructType
-from pyspark.sql.functions import isnan, when, count, datediff, mean, lag
+from pyspark.sql.types import TimestampType, DoubleType, StringType, \
+    StructField, StructType
+from pyspark.sql.functions import isnan, when, count, datediff, mean, lag, col, avg, variance, stddev
 from pyspark.sql.window import Window
 
 
@@ -28,7 +32,7 @@ def load_data(spark_session: SparkSession, path: str) -> DataFrame:
         StructField("Low", DoubleType()),
         StructField("Open", DoubleType()),
         StructField("Close", DoubleType()),
-        StructField("Volume", LongType()),
+        StructField("Volume", DoubleType()),
         StructField("Adj Close", DoubleType()),
         StructField("company_name", StringType()),
     ])
@@ -40,19 +44,23 @@ def count_nan(data_frame: DataFrame) -> DataFrame:
     Count nan in the <df> DataFrame
     """
     return data_frame.select([
-                count(when(isnan(c), c)).alias(c)
-                for c
-                in ["High", "Low", "Open", "Close", "Volume", "Adj Close"]
-              ])
+        count(when(isnan(c) | col(c).isNull(), c)).alias(c)
+        for c
+        in ["High", "Low", "Open", "Close", "Volume", "Adj Close"]
+    ])
 
 
-def duration_between_rows(df: DataFrame):
-    df = df.orderBy('Date')
-    df = df.withColumn('duration',
-                       datediff(df['Date'],
-                                lag(df['Date'], 1).over(Window.partitionBy("company_name").orderBy('Date')))
-                       )
-    return df.select(mean('duration')).collect()[0][0]
+def duration_between_rows(data_frame: DataFrame):
+    """
+    Calculate the mean duration between each rows
+    """
+    data_frame = data_frame.orderBy('Date')
+    data_frame = data_frame.withColumn('duration',
+                                       datediff(data_frame['Date'],
+                                                lag(data_frame['Date'], 1).over(
+                                                    Window.partitionBy("company_name").orderBy('Date')))
+                                       )
+    return data_frame.select(mean('duration')).collect()[0][0]
 
 
 def corr_two_columns(data_frame: DataFrame, col1: str, col2: str) -> float:
@@ -69,8 +77,8 @@ def corr_matrix(data_frame: DataFrame) -> DenseMatrix:
 
     vector_col = "corr_features"
     assembler = VectorAssembler(
-            inputCols=["High", "Low", "Open", "Close", "Volume", "Adj Close"],
-            outputCol=vector_col
+        inputCols=["High", "Low", "Open", "Close", "Volume", "Adj Close"],
+        outputCol=vector_col
     )
     df_vector = assembler.transform(data_frame).select(vector_col)
 
@@ -79,23 +87,73 @@ def corr_matrix(data_frame: DataFrame) -> DenseMatrix:
     return matrix
 
 
-if __name__ == "__main__":
-    spark = create_spark_session("Spark_Application_Name")
-    df = load_data(spark, 'stocks_data/AMAZON.csv')
+def compute_moving_average(stock_history: DataFrame, column: str, n: int = 5) -> DataFrame:
+    ordered_window = Window.partitionBy("company_name").orderBy("Date").rowsBetween(-1 * n, 0)
+    return stock_history.withColumn("daily_average", avg(stock_history[column]).over(ordered_window))
 
-    df.printSchema()
-    df.show(40)
-    df.count()
-    print(duration_between_rows(df))
+
+def compute_volatility(df: DataFrame, start_date: TimestampType(), end_date: TimestampType(), n: int = 5):
+    selected_df = df.filter(col("Date") <= start_date).filter(col("Date") <= end_date)
+    selected_w_average = compute_moving_average(selected_df, "Close", n)
+    return selected_w_average.select(stddev("daily_average"))
+
+
+def compute_momentum(stock_history: DataFrame, n: int = 5) -> DataFrame:
+    ordered_window = Window.partitionBy("company_name").orderBy("Date").rowsBetween(-1 * n, 0)
+    return stock_history.withColumn("daily_momentum", avg(stock_history["Close"]).over(ordered_window))
+
+
+def describe_data_frame(data_frame: DataFrame):
+    """
+    Describe the dataframe
+    - print the first and last 40 lines
+    - print the number of observations
+    - print the period between the data points
+    - print the min, max, mean and standard deviation
+    - print the number of missing values for each dataframe and column
+    - print correlation matrix
+    """
+    print("Dataframe schema:")
+    data_frame.printSchema()
+
+    print("First 40 lines:")
+    data_frame.show(40)
+
+    print(f"Number of observations: {data_frame.count()}\n")
+
+    print("Period between data points:")
+    print(duration_between_rows(data_frame))
 
     # Descriptive statistics for each dataframe and each column (min, max,
     # standard deviation)
-    df.describe().show()
+    data_frame.describe().show()
 
-    # Number of missing values for each dataframe and column
-    count_nan(df).show()
+    print("Number of missing values for each dataframe and column:")
+    count_nan(data_frame).show()
 
-    # Correlation between values
-    pearsonCorr = corr_two_columns(df, "High", "Low")
-    corr = corr_matrix(df)
-    print(corr)
+    print("Correlation between 'High' and 'Low':")
+    pearson_corr = corr_two_columns(data_frame, "High", "Low")
+    print(pearson_corr)
+    print("Correlation matrix:")
+    corr = corr_matrix(data_frame)
+    print(corr, '\n')
+
+    print("Moving Average")
+    avgdf = compute_moving_average(data_frame, "Close")
+    avgdf.show()
+
+    print("volatility")
+    format = "%Y-%m-%d %H:%M:%S"
+    start = datetime.datetime.strptime("2019-04-18 00:00:00", format)
+    end = datetime.datetime.strptime("2019-05-16 00:00:00", format)
+    vol = compute_volatility(df, start, end)
+    vol.show()
+
+
+if __name__ == "__main__":
+    spark = create_spark_session("Spark_Application_Name")
+    for f in ['AMAZON.csv', 'APPLE.csv', 'FACEBOOK.csv', 'GOOGLE.csv',
+              'MICROSOFT.csv', 'TESLA.csv', 'ZOOM.csv']:
+        print(f"\n{f}:")
+        df = load_data(spark, 'stocks_data/' + f)
+        describe_data_frame(df)
