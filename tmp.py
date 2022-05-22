1e6a17ff37897d778ff120a5f510a270d385eccb
mehdi "Arcanite" Oueslati <mehdi.oueslati@epita.fr>
2022-05-23 01:32:19 +0200

    extra indicators

--- tmp.py
+++ tmp.py
@@ -11,7 +11,7 @@ from pyspark.ml.stat import Correlation
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import TimestampType, DoubleType, StringType, \
     StructField, StructType
-from pyspark.sql.functions import isnan, when, count, datediff, mean, lag, col, avg, variance, stddev
+from pyspark.sql.functions import isnan, when, count, datediff, mean, lag, col, avg, variance, stddev, asc, desc, sum
 from pyspark.sql.window import Window
 
 
@@ -99,8 +99,7 @@ def compute_volatility(df: DataFrame, start_date: TimestampType(), end_date: Tim
 
 
 def compute_momentum(stock_history: DataFrame, n: int = 5) -> DataFrame:
-    ordered_window = Window.partitionBy("company_name").orderBy("Date").rowsBetween(-1 * n, 0)
-    return stock_history.withColumn("daily_momentum", avg(stock_history["Close"]).over(ordered_window))
+    return compute_moving_average(stock_history, "Close", n)
 
 
 def upside_downside_ratio(stocks: list[DataFrame]) -> DataFrame:
@@ -115,9 +114,9 @@ def upside_downside_ratio(stocks: list[DataFrame]) -> DataFrame:
         all_joined = all_joined.join(df_next, on='Date', how='inner')
 
     def gen_pos(frame: DataFrame, pred: callable):
-        res = all_joined["advancement_0"] * (all_joined["advancement_0"] > 0).cast("double")
+        res = frame["advancement_0"] * (frame["advancement_0"] > 0).cast("double")
         for index in range(len(stocks) - 1):
-            res += all_joined[f"advancement_{index + 1}"] * (pred(all_joined[f"advancement_{index+1}"])).cast("double")
+            res += frame[f"advancement_{index + 1}"] * (pred(frame[f"advancement_{index + 1}"])).cast("double")
         return res
 
     out = all_joined.withColumn("total_upside", gen_pos(all_joined, lambda x: x > 0))
@@ -126,6 +125,89 @@ def upside_downside_ratio(stocks: list[DataFrame]) -> DataFrame:
     out = out.select("Date", "upside_downside_ratio")
     return out
 
+
+def compute_average_over_period(frame: DataFrame, col_name: str, start_date: TimestampType(),
+                                end_date: TimestampType()):
+    return frame.filter(col("Date") >= start_date).filter(col("Date") <= end_date).agg(avg(col(col_name)))
+
+
+def compute_daily_return(frame: DataFrame) -> DataFrame:
+    return frame.withColumn("daily_return", (frame["Close"] - frame["Open"]) / frame["Open"] * 100)
+
+
+def compute_return_rate_over_period(frame: DataFrame, start_date: TimestampType(), end_date: TimestampType()):
+    curated_frame = frame.filter(col("Date") >= start_date).filter(col("Date") <= end_date)
+    open_val = curated_frame.filter(col("Date") == start_date).head(1)[0].__getitem__("Open")
+    close_val = curated_frame.filter(col("Date") == end_date).head(1)[0].__getitem__("Close")
+    return (close_val - open_val) / open_val
+
+
+def highest_daily_return(stocks: list[DataFrame]) -> DataFrame:
+    stocks_w_daily_return = list(map(lambda x: compute_daily_return(x), stocks))
+    all_joined = stocks_w_daily_return[0]
+    for df_next in stocks_w_daily_return[1:]:
+        all_joined = all_joined.union(df_next)
+    return all_joined.sort(desc('daily_return')).dropDuplicates(['date']).select("Date", "company_name", "daily_return")
+
+
+def highest_period_return(stocks: list[DataFrame], start_date: TimestampType(), end_date: TimestampType()):
+    highest_val = compute_return_rate_over_period(stocks[0], start_date, end_date)
+    name = stocks[0].head(1)[0].__getitem__("company_name")
+    for stock in stocks[1:]:
+        return_rate = compute_return_rate_over_period(stock, start_date, end_date)
+        if return_rate > highest_val:
+            highest_val = return_rate
+            name = stock.head(1)[0].__getitem__("company_name")
+    return name, highest_val
+
+
+def compute_intraday_momentum_index(stock: DataFrame, n: int = 14):
+    ordered_window = Window.partitionBy("company_name").orderBy("Date").rowsBetween(-1 * n, 0)
+    stock_w_daily_gains = stock.withColumn("daily_gain", stock["Close"] - stock["Open"])
+    stock_w_daily_gains.show()
+    stock_w_daily_gl = stock_w_daily_gains.withColumn("daily_loss",
+                                                      stock_w_daily_gains["Open"] - stock_w_daily_gains["Close"])
+    stock_w_daily_gbool = stock_w_daily_gl.withColumn("gain_bool",
+                                                      (stock_w_daily_gl["Close"] > stock_w_daily_gl["Open"])
+                                                      .cast("Double"))
+    stock_w_daily_glbool = stock_w_daily_gbool.withColumn("loss_bool",
+                                                          (stock_w_daily_gbool["Open"] > stock_w_daily_gbool["Close"])
+                                                          .cast("Double"))
+    stock_w_daily_netg = stock_w_daily_glbool.withColumn("net_gain", stock_w_daily_glbool["daily_gain"] *
+                                                         stock_w_daily_glbool["gain_bool"])
+    stock_w_daily_netgl = stock_w_daily_netg.withColumn("net_loss", stock_w_daily_netg["daily_loss"] *
+                                                         stock_w_daily_glbool["loss_bool"])
+    stock_w_momentum_gains = stock_w_daily_netgl.withColumn("mgains", sum(stock_w_daily_netgl["net_gain"])
+                                                            .over(ordered_window))
+    stock_w_mgl = stock_w_momentum_gains.withColumn("mlosses", sum(stock_w_momentum_gains["net_loss"])
+                                                    .over(ordered_window))
+    stock_w_imi = stock_w_mgl.withColumn("imi", stock_w_mgl["mgains"] /
+                                         (stock_w_mgl["mgains"] + stock_w_mgl["mlosses"]) * 100)
+    return stock_w_imi
+
+
+def compute_money_flow_index(stock: DataFrame, n: int = 14):
+    ordered_window = Window.partitionBy("company_name").orderBy("Date").rowsBetween(-1 * n, 0)
+    one_window = Window.partitionBy("company_name").orderBy("Date")
+    stock_w_typical = stock.withColumn("typical_price", stock["High"] + stock["Low"] + stock["Close"])
+    stock_w_raw = stock_w_typical.withColumn("raw_money_flow", stock_w_typical["typical_price"] * stock_w_typical["Volume"])
+    stock_w_is_pos = stock_w_raw.withColumn("is_pos_flow",
+                                            (lag(stock_w_raw["typical_price"]).over(one_window)
+                                             < stock_w_raw["typical_price"])
+                                            .cast("Double"))
+    stock_w_ispl = stock_w_is_pos.withColumn("is_neg_flow",
+                                              (lag(stock_w_is_pos["typical_price"]).over(one_window)
+                                               > stock_w_is_pos["typical_price"])
+                                              .cast("Double"))
+    stock_w_period_pos = stock_w_ispl.withColumn("pos_period_flow",
+                                                 sum(stock_w_ispl["is_pos_flow"] * stock_w_ispl["raw_money_flow"])
+                                                 .over(ordered_window))
+    stock_w_period_pn = stock_w_period_pos.withColumn("neg_period_flow",
+                                                        sum(stock_w_period_pos["is_neg_flow"] * stock_w_period_pos["raw_money_flow"])
+                                                    .over(ordered_window))
+    stock_w_ratio = stock_w_period_pn.withColumn("money_flow_ratio",
+                                                 stock_w_period_pn["pos_period_flow"] / stock_w_period_pn["neg_period_flow"])
+    return stock_w_ratio.withColumn("money_flow_index", 100 / (1 + stock_w_ratio["money_flow_ratio"]))
 
 def describe_data_frame(data_frame: DataFrame):
     """
@@ -167,11 +249,23 @@ def describe_data_frame(data_frame: DataFrame):
     avgdf.show()
 
     print("volatility")
-    format = "%Y-%m-%d %H:%M:%S"
-    start = datetime.datetime.strptime("2019-04-18 00:00:00", format)
-    end = datetime.datetime.strptime("2019-05-16 00:00:00", format)
+    format_date = "%Y-%m-%d %H:%M:%S"
+    start = datetime.datetime.strptime("2019-04-18 00:00:00", format_date)
+    end = datetime.datetime.strptime("2019-05-16 00:00:00", format_date)
     vol = compute_volatility(df, start, end)
     vol.show()
+    print(f"average opening from {start} to {end}")
+    compute_average_over_period(df, "Open", start, end).show()
+    print(f"average closing from {start} to {end}")
+    compute_average_over_period(df, "Close", start, end).show()
+    print("daily return rate")
+    compute_daily_return(df).show()
+    print(f"return rate from {start} to {end}")
+    print(compute_return_rate_over_period(df, start, end))
+    print("IMI score")
+    compute_intraday_momentum_index(df).show()
+    print("Money Flow Index")
+    compute_money_flow_index(df).show()
 
 
 if __name__ == "__main__":
@@ -185,3 +279,10 @@ if __name__ == "__main__":
         describe_data_frame(df)
     print("upside_downside_ratio")
     upside_downside_ratio(all_datasets).show()
+    print("highest daily return")
+    highest_daily_return(all_datasets).show()
+    date_format = "%Y-%m-%d %H:%M:%S"
+    start = datetime.datetime.strptime("2019-04-18 00:00:00", date_format)
+    end = datetime.datetime.strptime("2019-05-16 00:00:00", date_format)
+    print(f"highest return from {start} to {end}")
+    print(highest_period_return(all_datasets, start, end))
